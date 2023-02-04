27701edf642a88fd14008c25e0499feca144c13f
miaou33 <nfauconn@student.42.fr>
2023-02-04 14:00:24 +0100

    ex05 prototypes

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,25 @@
+#include "Harl.hpp"
+
+void	Harl::debug (void) {
+
+	std::cout << "my love having extra bacon" << std::endl;
+}
+
+void	Harl::info (void) {
+
+	std::cout << "wut ? extra bacon costs more money" << std::endl;
+}
+
+void	Harl::warning (void) {
+
+	std::cout << "give me bacon for free nah ? i come often" << std::endl;
+}
+
+void	Harl::error (void) {
+
+	std::cout << "hate u. wanna speak to manager rn" << std::endl;
+}
+
+void	Harl::complain (std::string level) {
+
+}
