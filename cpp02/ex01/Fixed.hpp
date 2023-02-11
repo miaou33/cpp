b684e983c591a19f40e218c899e1ece80cb2cb86
miaou33 <nfauconn@student.42.fr>
2023-02-11 17:56:07 +0100

    clean .cpp

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -12,16 +12,22 @@ class	Fixed {
 		static int const	_nb_bits_fractionnal = 8;
 
 	public:
+
+		// CONSTRUCTORS
 		Fixed ();
 		Fixed (Fixed const &copy);
 		Fixed (int const to_convert);
 		Fixed (float const toFix);
 		~Fixed ();
 
+		// OPERATORS
 		Fixed &operator = (Fixed const &rhs);
 
+		// GETTERS / SETTERS
 		int 	getRawBits (void) const;
 		void	setRawBits (int const raw);
+		
+		// OTHERS
 		int		toInt (void) const;
 		float	toFloat (void) const;
 };
