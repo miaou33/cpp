452e70b626d55ca4e32cb6bd1ee317c20389764c
miaou33 <nfauconn@student.42.fr>
2023-02-11 17:29:47 +0100

    .gitignore

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <string>
+# include <iostream>
+
+class	Fixed {
+
+	private:
+		int					_val;
+		static int const	_nb_bits_fractionnal = 8;
+
+	public:
+		Fixed ();
+		Fixed (Fixed const &copy);
+		~Fixed ();
+
+		Fixed &operator = (Fixed const &rhs);
+
+		int 	getRawBits (void) const;
+		void	setRawBits (int const raw);
+};
+
+#endif
