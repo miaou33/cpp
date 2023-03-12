4ce3d400dc09ce79ad0122ea3caf287e2d43b820
miaou33 <nfauconn@student.42.fr>
2023-03-12 12:54:48 +0100

    cpp04 - ex02

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -7,6 +7,7 @@ int	main (void)
 {
 	int evensize = 4;
  	AAnimal	*animalArray [evensize];
+//	AAnimal	*animal = new AAnimal (); -> wrong car impossible d instancier une classe abstraite
 	int		arrayHalf = evensize / 2;
 
 	for (int i = 0; i < arrayHalf; i++)
@@ -14,10 +15,10 @@ int	main (void)
 		animalArray [i] = new Dog ();
 		animalArray [arrayHalf + i] = new Cat ();
 	}
-	animalArray[arrayHalf - 1]->makeSound ();
-	animalArray[arrayHalf + 1]->makeSound ();
+	animalArray[evensize % 2]->makeSound ();
+	animalArray[arrayHalf]->makeSound ();
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < evensize; i++)
 		delete animalArray [i];
 	return 0;
 }
