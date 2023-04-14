2d2036448f5f2dbfbf00be6b9101ba7d2b510421
no3 <meow@no3arch>
2023-04-14 13:00:10 +0200

    f

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -15,28 +15,82 @@ ScalarConverter& ScalarConverter::operator= (ScalarConverter const& rhs) {
     (void) rhs;
     return *this;
 }
+
 /******************************************************************************************************/
-/*    CONVERT                                                                                         */
+/*    GETTERS SETTERS                                                                                 */
 /******************************************************************************************************/
 
-void                ScalarConverter::convert (std::string const& input) {
+std::string const&  ScalarConverter::setParam () { return _param; }
+
+size_t              ScalarConverter::setParamLen () { return _param_len; }
+
+int                 ScalarConverter::setType () { return _type; }
+
+char                ScalarConverter::setCharCast () { return _c; }
+
+int                 ScalarConverter::setIntCast () { return _i; }
+
+double              ScalarConverter::setDoubleCast () { return _d; }
+
+float               ScalarConverter::setFloatCast () { return _f; }
+
+void                ScalarConverter::initCasts () {
+
+    _type = noType;
+    _c = 0;
+    _i = 0;
+    _d = 0.0;
+    _f = 0.0f;
+}
+
+/******************************************************************************************************/
+/*    EXCEPTIONS                                                                                      */
+/******************************************************************************************************/
+
+char const*            ScalarConverter::WrongArgument::what () const throw () {
+
+    return ("Wrong argument. Format : ./convert <literal>");
+}
+
+char const*            ScalarConverter::InvalidLitteral::what () const throw () {
+
+    return ("Invalid string. Please enter a literal thas is either a char, an int, a float or a double");
+}
+
+char const*            ScalarConverter::Impossible::what () const throw () {
+
+    return ("Impossible");
+}
+
+char const*            ScalarConverter::NonDisplayable::what () const throw () {
+
+    return ("Non displayable");
+}
+
+yu
+
+/******************************************************************************************************/
+/*    DISPLAY                                                                                         */
+/******************************************************************************************************/
+
+void                ScalarConverter::displayCasts () {
 
     try {
-        int type = findType (input);
+        findType ();
         errno = 0;
         switch (_type)
         {
             case charType:
-                _c = input [0];
+                _c = _param [0];
                 break;
             case intType:
-                _i = atoi (input.c_str ());
+                _i = atoi (_param.c_str ());
                 break;
             case floatType:
-                _f = std::strtof (input.c_str (), NULL);
+                _f = std::strtof (_param.c_str (), NULL);
                 break;
             case doubleType:
-                _d = std::strtod (input.c_str (), NULL);
+                _d = std::strtod (_param.c_str (), NULL);
                 break;
             default:
                 throw InvalidLitteral ();
@@ -51,17 +105,168 @@ void                ScalarConverter::convert (std::string const& input) {
     }
 }
 
-int                ScalarConverter::findType (std::string const& input) {
+/******************************************************************************************************/
+/*    CONVERT                                                                                         */
+/******************************************************************************************************/
 
-    switch (input.length ())
-    {
-        case 0:
-            return charType;
-        case 1:
-            return (std::isdigit (input [0]) ? intType : charType);
-        default:
-            return (input.find_first_not_of ("-0123456789.f") != std::string::npos ?
-                specialParse () 
-                : digitParse ());
+void                ScalarConverter::convert () {
+    
+    toChar ();
+    toInt ();
+    toFloat ();
+    toDouble ();
+}
+
+void                ScalarConverter::toChar () {
+
+    try {
+        announce ("Char: ");
+        switch (_type)
+        {
+            case charType:
+                display (_c);
+                break;
+            case intType:
+                isascii (_i) ?
+					display (static_cast <char> (_i)) : throw Impossible ();
+                break;
+            case floatType:
+                isascii (static_cast <int> (_f)) ?
+					display (static_cast <char> (_f)) : throw Impossible (); 
+                break;
+            case doubleType:
+                isascii (static_cast <int> (_d)) ?
+					display (static_cast <char> (_d)) : throw Impossible (); 
+                break;
+            default:
+                throw InvalidLitteral ();
+                break;
+        }
+    }
+    catch (std::exception& e) { 
+        display_exception (e); 
     }
 }
+
+void                ScalarConverter::toInt () {
+
+    try {
+        announce ("Int: ");
+        switch (_type)
+        {
+            case charType:
+                display (static_cast <int> (_c));
+                break;
+            case intType:
+                display (_i);
+                break;
+            case floatType:
+                (_f >= (float) INT_MIN && _f <= (float) INT_MAX) ?
+					display (static_cast <int> (_f)) : throw Impossible ();
+                break;
+            case doubleType:
+                (_d >= (double) INT_MIN && _d <= (double) INT_MAX) ? 
+					display (static_cast <int> (_d)) : throw Impossible (); 
+                break;
+            default:
+                throw InvalidLitteral ();
+                break;
+        }
+    }
+    catch (std::exception& e) { 
+        display_exception (e); 
+    }
+}
+
+void                ScalarConverter::toFloat () {
+
+    try {
+        announce ("Float: ");
+        switch (_type)
+        {
+            case charType:
+                display (static_cast <float> (_c)); 
+                break;
+            case intType:
+                display (static_cast <float> (_i)); 
+                break;
+            case floatType:
+                display (_f);
+                break;
+            case doubleType:
+                (_d >= FLT_MIN && _d <= FLT_MAX) ?
+                    display (static_cast <float> (_d)) : throw Impossible (); 
+                break;
+            default:
+                throw InvalidLitteral ();
+                break;
+        }
+    }
+    catch (std::exception& e) { 
+        display_exception (e); 
+    }
+}
+
+void                ScalarConverter::toDouble () {
+
+    try {
+        announce ("Double: ");
+        switch (_type)
+        {
+            case charType:
+                display (static_cast <double> (_c)); 
+                break;
+            case intType:
+                display (static_cast <double> (_i)); 
+                break;
+            case floatType:
+                display (static_cast <double> (_f)); 
+                break;
+            case doubleType:
+                display (_d);
+                break;
+            default:
+                throw InvalidLitteral ();
+                break;
+        }
+    }
+    catch (std::exception& e) { 
+        display_exception (e); 
+    }
+}
+
+void                ScalarConverter::findType () {
+
+    switch (_param_len)
+    {
+        case 0:
+            _type = charType;
+            break;
+        case 1:
+            _type = std::isdigit (_param [0]) ? intType : charType;
+            break;
+        default:
+            _param.find_first_not_of ("-0123456789.f") != std::string::npos ? specialParse () : digitParse ();
+    }
+}
+
+void                ScalarConverter::digitParse () {
+
+    neg_parse (_param); 
+    _type = (float_parse (_param, _param_len) == true) ?
+		floatType : _type;
+    _type = (_type == noType && double_parse (_param) == true) ?
+		doubleType : _type;
+    _type = (_type == noType && int_parse (_param) == true) ?
+		intType : _type;
+}
+
+void                ScalarConverter::specialParse () {
+
+    special_float_double_parse (_param);
+    _type = std::strtof (_param.c_str (), NULL) ?
+		floatType : _type;
+    _type = (_type == noType && std::strtod (_param.c_str (), NULL)) ?
+		doubleType : _type;
+}
+
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -8,16 +8,6 @@
 # include <stdio.h>
 # include <string>
 
-# include "utils.hpp"
-
-enum e_type {
-                noType = -1,
-                charType,
-                intType,
-                floatType,
-                doubleType
-            };
-
 class ScalarConverter {
 
     public:
@@ -25,18 +15,20 @@ class ScalarConverter {
         ScalarConverter ();
         ScalarConverter (std::string const& string);
         ScalarConverter (ScalarConverter const& original);
-        ~ScalarConverter ();
+        virtual ~ScalarConverter ();
 
         ScalarConverter&    operator= (ScalarConverter const& rhs);
 
-        static void         convert (std::string const& input);
-        static int          findType (std::string const& input);
+        static void         setParam ();
+        static void         setParamLen ();
+        static void         setType ();
+        static void         setCharCast ();
+        static void         setIntCast ();
+        static void         setDoubleCast ();
+        static void         setFloatCast ();
 
-        operator char ();
-        operator int ();
-        operator float ();
-        operator double ();
-
+        void                displayCasts ();
+    
         class WrongArgument : public std::exception {
 
             virtual char const*    what () const throw ();
@@ -57,7 +49,51 @@ class ScalarConverter {
 
             virtual char const*    what () const throw ();
         };
+
+
+    private:
+
+        enum e_type {
+            noType = -1,
+            charType,
+            intType,
+            floatType,
+            doubleType
+        };
+
+        static std::string     _param;
+        static size_t          _param_len;
+        static int             _type;
+        static char            _c;
+        static int             _i;
+        static float           _f;
+        static double          _d;
+
+        void            initCasts ();
+        void            findType ();
+        void            specialParse ();
+        void            digitParse ();
+
+        void            convert ();
+        void            toChar ();
+        void            toInt ();
+        void            toFloat ();
+        void            toDouble ();
 };
 
+
+void    special_float_double_parse (std::string const& s);
+void    neg_parse (std::string const& s);
+bool    float_parse (std::string const& s, size_t s_len);
+bool    double_parse (std::string const& s);
+bool    int_parse (std::string const& s);
+
+void    announce (std::string const& s);
+void    display (char c);
+void    display (int i);
+void    display (float f);
+void    display (double d);
+void    display (std::string const& s);
+void    display_exception (std::exception& e);
 
 # endif
