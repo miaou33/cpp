e9d07245ff7346c4f664bcda2e8a8218b000bf05
miaou33 <nfauconn@student.42.fr>
2023-04-28 16:40:45 +0200

    array.hpp

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,25 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <string>
+# include <iostream>
+
+<<<<<<< HEAD
+template <class T>
+class Array {
+
+	public:
+		Array ();
+		Array (Array const& original);
+		~Array ();
+
+		Array const&	operator= (Array const& rhs);
+	
+		T array;
+
+	private:
+}
+=======
+>>>>>>> refs/remotes/origin/main
+
+# endif
