0f5bc91b6ce080cfb58e3f68c3d042d273cf4e70
no3 <meow@no3arch>
2023-04-30 19:10:43 +0200

    added more tests (.tpp?)

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -4,17 +4,26 @@ int main () {
 
 	try {
 
-		//Array <int> nullArray;
-		//std::cout << "initialized nullArray = " << nullArray << std::endl;
+		Array <int> nullArray;
+		std::cout << "initialized nullArray =\n" << nullArray << std::endl;
 		//std::cout << nullArray [8] << std::endl;
 
-		t_ui	n = 5;
+		t_ui	n = 3;
 		Array <int> intArray (n);
 		Array <int> intArrayCopy (intArray);
 		intArray [2] = 3;
-		std::cout << "intArray = " << intArray << std::endl;
-		std::cout << "intArrayCopy= " << intArrayCopy << std::endl;
+		std::cout << "intArray =\n" << intArray << std::endl;
+		std::cout << "intArrayCopy=\n" << intArrayCopy << std::endl;
 
+		Array <std::string> strArray (n);
+		Array <std::string> strArrayCopy (n);
+		std::cout << "strArray =\n" << strArray << std::endl;
+		std::cout << "strArrayCopy =\n" << strArrayCopy << std::endl;
+		strArray [0] = "ddd";
+		strArray [2] = "miao";
+		//strArray [4] = "bla";
+		std::cout << "strArray =\n" << strArray << std::endl;
+		std::cout << "strArrayCopy =\n" << strArrayCopy << std::endl;
 
 	}
 	catch (std::out_of_range& o) {
