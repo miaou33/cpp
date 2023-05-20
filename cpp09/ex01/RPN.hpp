e94fb0160e93b526e88772f0812e5da74aedced5
no3 <meow@no3arch>
2023-05-20 15:21:05 +0200

    ex01

---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,20 @@
+#ifndef RPN_HPP
+# define RPN_HPP
+
+# include <string>
+# include <iostream>
+
+class RPN {
+
+	public:
+
+		RPN ();
+		RPN (RPN const& original);
+		RPN& operator= (RPN const& rhs);
+		~RPN ();
+
+	private:
+
+};
+
+# endif
